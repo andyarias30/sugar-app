209168cd62b29943d86ce60403d4c171bd7dfe2f
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2023-11-28 09:21:33 -0500

    table

--- src/components/SugarLevel.jsx
+++ src/components/SugarLevel.jsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react"
 import Table from 'react-bootstrap/Table';
-
-
 export default function SugarLevels(){
     const [sugarLevels, setSugarLervels] = useState([])
     useEffect(() => {
@@ -12,60 +10,36 @@ export default function SugarLevels(){
         .catch(err => console.error(err))
     },[]) 
         
-    return(
-        <div>
-            {sugarLevels.map(level => {
-                return <div key={level._id}>{level.userName}</div>
-            })
-            }
-
-
-            
-            {/* "_id": "6564ee66672aa65e15f1cdd1",
-        "userName": "Andy",
-        "beforeBreakfast": 90,
-        "beforeLunch": 98,
-        "beforeDinner": 98 */}
-        </div>
-    )
-    
-}
-
-
-  return (
-    <Table responsive>
-      <thead>
-        <tr>
-          <th>#</th>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <th key={index}>Table heading</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>2</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-        <tr>
-          <td>3</td>
-          {Array.from({ length: 12 }).map((_, index) => (
-            <td key={index}>Table cell {index}</td>
-          ))}
-        </tr>
-      </tbody>
-    </Table>
-  );
-
-
-
-
-
+    return (
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>#</th>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <th key={index}>Table heading</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <td key={index}>Table cell {index}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>2</td>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <td key={index}>Table cell {index}</td>
+              ))}
+            </tr>
+            <tr>
+              <td>3</td>
+              {Array.from({ length: 12 }).map((_, index) => (
+                <td key={index}>Table cell {index}</td>
+              ))}
+            </tr>
+          </tbody>
+        </Table>
+      );
+    }
